0bf728aef77bde1bd6a515e8c3f18f8c5f7c76ca
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-24 18:39:20 +0000

    services able to communicate

--- backEnd/backEndApp.py
+++ backEnd/backEndApp.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify
-from random import choice
+import random
 app = Flask(__name__)
 
 animals = {'cow':'moo', 'dog':'woof', 'cat':'meow', 'sheep':'baaaa', 'tom':'AAAAAAAAAAA'}
 
 @app.route('/animal')
 def get_animal():
-    return jsonify({"data":choice(animals).key()})
+    return jsonify({'data':random.choice(list(animals))})
 
 @app.route('/noise/<string:animal>', methods=['POST'])
 def get_sound(animal):
-    return jsonify(animals.get(animal))
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+animal)
+    return jsonify({'data':animals[animal]})
 
 if __name__ == '__main__':
-    app.run(port=5001, host='0.0.0.0/0') 
+    app.run(port=5001, host='0.0.0.0') 
--- frontEnd/frontEndApp.py
+++ frontEnd/frontEndApp.py
@@ -6,8 +6,8 @@ api = 'http://localhost:5001'
 @app.route('/')
 def index():
     animal = requests.get(api + '/animal')
-    sound = requests.post(api + '/noise/' + animal)
-    return("the " + str(animal.json()["data"]) + " goes " + str(sound))
+    sound = requests.post(api + '/noise/' + str(animal.json()["data"]))
+    return("the " + str(animal.json()["data"]) + " goes " + str(sound.json()["data"]))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
