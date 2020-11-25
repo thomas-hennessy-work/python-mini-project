9a03d49874e6a88e9f67fc6d705b0351598e468b
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-25 16:05:18 +0000

    removed line numbers

--- frontEndUnitTests.sh
+++ frontEndUnitTests.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-echo line 1
 docker build -f frontEnd/Dockerfile -t appfrontend:miniproject .
-
-echo line 2
 docker run -d --name frontendunittests appfrontend:miniproject 
-
-echo line 3
 docker exec -it frontendunittests bash -c "pytest"
-
-echo line 4
 docker stop frontendunittests
-echo line 5
 docker rm frontendunittests
-echo line 6
 docker rmi appfrontend:miniproject
