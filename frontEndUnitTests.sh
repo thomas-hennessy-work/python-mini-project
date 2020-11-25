d63afb4f6f3441cc5585a27e85ee0cb149f6c42e
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-25 16:07:13 +0000

    made the exec non-interactive

--- frontEndUnitTests.sh
+++ frontEndUnitTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -f frontEnd/Dockerfile -t appfrontend:miniproject .
 docker run -d --name frontendunittests appfrontend:miniproject 
-docker exec -it frontendunittests bash -c "pytest"
+docker exec frontendunittests bash -c "pytest"
 docker stop frontendunittests
 docker rm frontendunittests
 docker rmi appfrontend:miniproject
