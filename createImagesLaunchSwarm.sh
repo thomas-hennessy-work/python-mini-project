5c4f787d960e2ff73f8c26db5b877fe1b515e230
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-25 08:42:17 +0000

    images created and stable, unable to connect

--- createImagesLaunchSwarm.sh
+++ createImagesLaunchSwarm.sh
@@ -1,3 +1,3 @@
 docker build -f frontEnd/Dockerfile -t appfrontend:miniproject .
 docker build -f backEnd/Dockerfile -t appbackend:miniproject .
-#docker stack deploy --compose-file docker-compose.yaml animal-app
+docker stack deploy --compose-file docker-compose.yaml animal-app
