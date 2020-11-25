385c614e174d2bc83873fa4afc809bb65294c381
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-25 16:03:47 +0000

    bash script for unit test container functional

--- frontEndUnitTests.sh
+++ frontEndUnitTests.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo line 1
 docker build -f frontEnd/Dockerfile -t appfrontend:miniproject .
 
+echo line 2
+docker run -d --name frontendunittests appfrontend:miniproject 
+
+echo line 3
+docker exec -it frontendunittests bash -c "pytest"
+
+echo line 4
 docker stop frontendunittests
+echo line 5
 docker rm frontendunittests
-
-docker run -d --name frontendunittests appfrontend:miniproject
-
-docker exec -it frontendunittests bash pytest
-
-docker stop frontendunittests
-docker rm frontendunittests
-docker rmi frontendunittests
+echo line 6
+docker rmi appfrontend:miniproject
