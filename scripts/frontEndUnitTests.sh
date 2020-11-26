4aaa88fafb5d08e2ef4e19a285c4cfd9e00d6b90
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-26 12:34:09 +0000

    changed file context for unit tests

--- scripts/frontEndUnitTests.sh
+++ scripts/frontEndUnitTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -f ../frontEnd/Dockerfile -t appfrontend:miniproject ../frontEnd
+docker build -f ../frontEnd/Dockerfile -t appfrontend:miniproject .
 docker run -d --name frontendunittests appfrontend:miniproject 
 docker exec frontendunittests bash -c "pytest"
 docker stop frontendunittests
