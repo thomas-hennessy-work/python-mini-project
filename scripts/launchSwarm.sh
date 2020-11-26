0066ce99b23d2804af80f30e1070dd3d83c4459f
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-26 16:06:00 +0000

    added steps to swarm script to allow for rolling updates

--- scripts/launchSwarm.sh
+++ scripts/launchSwarm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker build -f ./frontEnd/Dockerfile -t thomashennessy/appfrontend:miniproject ./frontEnd
 docker build -f ./backEnd/Dockerfile -t thomashennessy/appbackend:miniproject ./backEnd
+docker push thomashennessy/appfrontend:miniproject
+docker push thomashennessy/appbackend:miniproject
 docker stack deploy --compose-file docker-compose.yaml animal-app
